22f67666b1879378dad1aa4721dc5e9240b9a112
liyasthomas <liyascthomas@gmail.com>
2019-05-23 10:52:32 +0530

    :sparkles: Added category/subject filter, filter state prompt

--- index.js
+++ index.js
@@ -8,19 +8,33 @@ const getBooks = async book => {
 	return data
 }
 const drawListBook = async () => {
-	if (searchBooks.value !== '') {
+	if (searchBooks.value != '') {
+		bookContainer.innerHTML = 'Searching..'
 		const data = await getBooks(searchBooks.value)
 		bookContainer.innerHTML = data.items
 			.map(({
 				volumeInfo
-			}) => `<div class='book'><a href='${volumeInfo.infoLink}' target='_blank'><img class='thumbnail' src='${volumeInfo.imageLinks.thumbnail}'></a><div class='book-info'><a href='${volumeInfo.infoLink}' target='_blank'><h2 class='book-title'>${volumeInfo.title}</h2></a><div class='book-authors' onclick='updateAuthor(this);'>${volumeInfo.authors}</div></div></div>`)
+			}) => `<div class='book'><a href='${volumeInfo.infoLink}' target='_blank'><img class='thumbnail' src='${volumeInfo.imageLinks.thumbnail}'></a><div class='book-info'><a href='${volumeInfo.infoLink}' target='_blank'><h3 class='book-title'>${volumeInfo.title}</h3></a><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");'>${volumeInfo.categories}</div></div></div>`)
 			.join('')
+	} else {
+		bookContainer.innerHTML = 'Enter a search term'
 	}
 }
-const updateAuthor = (e) => {
-	searchBooks.value = `inauthor:` + e.innerHTML
+const updateFilter = ({
+	innerHTML
+}, f) => {
+	let m
+	switch (f) {
+		case 'author':
+			m = 'inauthor:'
+			break
+		case 'subject':
+			m = 'subject:'
+			break
+	}
+	searchBooks.value = m + innerHTML
 	debounce(drawListBook, 1000)
 }
 const debounce = (fn, time, to = null) =>
 	to ? clearTimeout(to) : (to = setTimeout(drawListBook, time))
-searchBooks.addEventListener('change', () => debounce(drawListBook, 1000))
+searchBooks.addEventListener('input', () => debounce(drawListBook, 1000))
