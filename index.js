c1a855b154ba9919fc2ca56a92484c67b99bb918
liyasthomas <liyascthomas@gmail.com>
2019-05-24 08:31:32 +0530

    :sparkles: Added categories to home page

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-let bookContainer = document.querySelector('.list-book')
+let bookContainer = document.querySelector('.search-book')
 let searchBooks = document.getElementById('search')
 const getBooks = async book => {
-	const response = await fetch(
-		`https://www.googleapis.com/books/v1/volumes?q=${book}`
-	)
+	const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}`)
 	const data = await response.json()
 	return data
+}
+const drawChartBook = async (subject) => {
+	let cbookContainer = document.querySelector(`.${subject}-book`)
+	cbookContainer.innerHTML = `<div class='prompt'><div class="loader"></div><div>Loading...</div></div>`
+	const cdata = await getBooks(`subject:${subject}&maxResults=6&orderBy=newest`)
+	if (cdata.error) {
+		cbookContainer.innerHTML = `<div class='prompt'><div class='prompt'>😕</div>Limit exceeded! Try after some time</div>`
+	} else if (cdata.totalItems == 0) {
+		cbookContainer.innerHTML = `<div class='prompt'><div class='prompt'>😕</div>No results, try a different term!</div>`
+	} else if (cdata.totalItems == undefined) {
+		cbookContainer.innerHTML = `<div class='prompt'><div class='prompt'>😕</div>Network problem!</div>`
+	} else {
+		cbookContainer.innerHTML = cdata.items
+			.map(({
+				volumeInfo
+			}) => `<div class='book'><a href='${volumeInfo.infoLink}' target='_blank'><img class='thumbnail' src='${volumeInfo.imageLinks.thumbnail}' onerror='this.src="icons/logo.svg";'></a><div class='book-info'><a href='${volumeInfo.infoLink}' target='_blank'><h3 class='book-title'>${volumeInfo.title}</h3></a><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");'>${volumeInfo.categories}</div></div></div>`)
+			.join('')
+	}
 }
 const drawListBook = async () => {
 	if (searchBooks.value != '') {
@@ -31,6 +47,9 @@ const drawListBook = async () => {
 const updateFilter = ({
 	innerHTML
 }, f) => {
+	document.getElementById('main').scrollIntoView({
+		behavior: 'smooth'
+	})
 	let m
 	switch (f) {
 		case 'author':
@@ -46,4 +65,12 @@ const updateFilter = ({
 const debounce = (fn, time, to = 0) => {
 	to ? clearTimeout(to) : (to = setTimeout(drawListBook, time))
 }
-searchBooks.addEventListener('input', () => debounce(drawListBook, 1000))
+searchBooks.addEventListener('input', () => {
+	debounce(drawListBook, 1000)
+})
+document.addEventListener('DOMContentLoaded', () => {
+	drawChartBook('fiction')
+	drawChartBook('poetry')
+	drawChartBook('fantasy')
+	drawChartBook('romance')
+})
