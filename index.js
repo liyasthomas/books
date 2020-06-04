8bfac3a2fff4a8f2a6a86159eee87aca5a5d98ae
Liyas Thomas <liyascthomas@gmail.com>
2020-06-04 23:40:16 +0530

    :rotating_light: Removing linter warnings

--- index.js
+++ index.js
@@ -1,203 +1,234 @@
-let bookContainer = document.querySelector('.search')
-let searchBooks = document.getElementById('search-box')
-const getBooks = async book => {
-	const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}`)
-	const data = await response.json()
-	return data
-}
+let bookContainer = document.querySelector(".search");
+let searchBooks = document.getElementById("search-box");
+const getBooks = async (book) => {
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${book}`
+  );
+  const data = await response.json();
+  return data;
+};
 
-const extractThumbnail = (volumeInfo) => {
-	const DEFAULT_THUMBNAIL = 'icons/logo.svg'
-	if (!volumeInfo.imageLinks || !volumeInfo.imageLinks.thumbnail) { return DEFAULT_THUMBNAIL }
-	return volumeInfo.imageLinks.thumbnail.replace('http://', 'https://')
-}
+const extractThumbnail = ({ imageLinks }) => {
+  const DEFAULT_THUMBNAIL = "icons/logo.svg";
+  if (!imageLinks || !imageLinks.thumbnail) {
+    return DEFAULT_THUMBNAIL;
+  }
+  return imageLinks.thumbnail.replace("http://", "https://");
+};
 const drawChartBook = async (subject, startIndex = 0) => {
-	let cbookContainer = document.querySelector(`.${subject}`)
-	cbookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`
-	const cdata = await getBooks(`subject:${subject}&startIndex=${startIndex}&maxResults=6`)
-	if (cdata.error) {
-		cbookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`
-	} else if (cdata.totalItems == 0) {
-		cbookContainer.innerHTML = `<div class='prompt'>ツ No results, try a different term!</div>`
-	} else if (cdata.totalItems == undefined) {
-		cbookContainer.innerHTML = `<div class='prompt'>ツ Network problem!</div>`
-	} else if (!cdata.items || cdata.items.length == 0) {
-		cbookContainer.innerHTML = `<div class='prompt'>ツ There is no more result!</div>`
-	} else {
-		cbookContainer.innerHTML = cdata.items
-		cbookContainer.innerHTML = cdata.items
-			.map(({
-				volumeInfo
-			}) => `<div class='book' style='background: linear-gradient(` + getRandomColor() + `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` + extractThumbnail(volumeInfo) + `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` + getRandomColor() + `;'>` + (volumeInfo.categories === undefined ? 'Others' : volumeInfo.categories) + `</div></div></div>`)
-			.join('')
-	}
-}
+  let cbookContainer = document.querySelector(`.${subject}`);
+  cbookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
+  const cdata = await getBooks(
+    `subject:${subject}&startIndex=${startIndex}&maxResults=6`
+  );
+  if (cdata.error) {
+    cbookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
+  } else if (cdata.totalItems == 0) {
+    cbookContainer.innerHTML = `<div class='prompt'>ツ No results, try a different term!</div>`;
+  } else if (cdata.totalItems == undefined) {
+    cbookContainer.innerHTML = `<div class='prompt'>ツ Network problem!</div>`;
+  } else if (!cdata.items || cdata.items.length == 0) {
+    cbookContainer.innerHTML = `<div class='prompt'>ツ There is no more result!</div>`;
+  } else {
+    cbookContainer.innerHTML = cdata.items;
+    cbookContainer.innerHTML = cdata.items
+      .map(
+        ({ volumeInfo }) =>
+          `<div class='book' style='background: linear-gradient(` +
+          getRandomColor() +
+          `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` +
+          extractThumbnail(volumeInfo) +
+          `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` +
+          getRandomColor() +
+          `;'>` +
+          (volumeInfo.categories === undefined
+            ? "Others"
+            : volumeInfo.categories) +
+          `</div></div></div>`
+      )
+      .join("");
+  }
+};
 const drawListBook = async () => {
-	if (searchBooks.value != '') {
-		bookContainer.style.display = 'flex'
-		bookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`
-		const data = await getBooks(`${searchBooks.value}&maxResults=6`)
-		if (data.error) {
-			bookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`
-		} else if (data.totalItems == 0) {
-			bookContainer.innerHTML = `<div class='prompt'>ツ No results, try a different term!</div>`
-		} else if (data.totalItems == undefined) {
-			bookContainer.innerHTML = `<div class='prompt'>ツ Network problem!</div>`
-		} else {
-			bookContainer.innerHTML = data.items
-				.map(({
-					volumeInfo
-				}) => `<div class='book' style='background: linear-gradient(` + getRandomColor() + `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` + extractThumbnail(volumeInfo) + `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` + getRandomColor() + `;'>` + (volumeInfo.categories === undefined ? 'Others' : volumeInfo.categories) + `</div></div></div>`)
-				.join('')
-		}
-	} else {
-		bookContainer.style.display = 'none'
-	}
-}
-const updateFilter = ({
-	innerHTML
-}, f) => {
-	document.getElementById('main').scrollIntoView({
-		behavior: 'smooth'
-	})
-	let m
-	switch (f) {
-		case 'author':
-			m = 'inauthor:'
-			break
-		case 'subject':
-			m = 'subject:'
-			break
-	}
-	searchBooks.value = m + innerHTML
-	debounce(drawListBook, 1000)
-}
+  if (searchBooks.value != "") {
+    bookContainer.style.display = "flex";
+    bookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
+    const data = await getBooks(`${searchBooks.value}&maxResults=6`);
+    if (data.error) {
+      bookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
+    } else if (data.totalItems == 0) {
+      bookContainer.innerHTML = `<div class='prompt'>ツ No results, try a different term!</div>`;
+    } else if (data.totalItems == undefined) {
+      bookContainer.innerHTML = `<div class='prompt'>ツ Network problem!</div>`;
+    } else {
+      bookContainer.innerHTML = data.items
+        .map(
+          ({ volumeInfo }) =>
+            `<div class='book' style='background: linear-gradient(` +
+            getRandomColor() +
+            `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` +
+            extractThumbnail(volumeInfo) +
+            `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` +
+            getRandomColor() +
+            `;'>` +
+            (volumeInfo.categories === undefined
+              ? "Others"
+              : volumeInfo.categories) +
+            `</div></div></div>`
+        )
+        .join("");
+    }
+  } else {
+    bookContainer.style.display = "none";
+  }
+};
+const updateFilter = ({ innerHTML }, f) => {
+  document.getElementById("main").scrollIntoView({
+    behavior: "smooth",
+  });
+  let m;
+  switch (f) {
+    case "author":
+      m = "inauthor:";
+      break;
+    case "subject":
+      m = "subject:";
+      break;
+  }
+  searchBooks.value = m + innerHTML;
+  debounce(drawListBook, 1000);
+};
 const debounce = (fn, time, to = 0) => {
-	to ? clearTimeout(to) : (to = setTimeout(drawListBook, time))
-}
-searchBooks.addEventListener('input', () => debounce(drawListBook, 1000))
-document.addEventListener('DOMContentLoaded', () => {
-	drawChartBook('love')
-	drawChartBook('feminism')
-	drawChartBook('inspirational')
-	drawChartBook('authors')
-	drawChartBook('fiction')
-	drawChartBook('poetry')
-	drawChartBook('fantasy')
-	drawChartBook('romance')
-})
-let mainNavLinks = document.querySelectorAll('.scrolltoview')
-window.addEventListener('scroll', event => {
-	let fromTop = window.scrollY + 64
-	mainNavLinks.forEach(({
-		hash,
-		classList
-	}) => {
-		let section = document.querySelector(hash)
-		if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
-			classList.add('current')
-		} else {
-			classList.remove('current')
-		}
-	})
-})
-const getRandomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}40`
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
-if (localStorage.getItem('marcdownTheme') == 'dark') {
-	document.documentElement.setAttribute('data-theme', 'dark')
-	document.querySelector('meta[name=theme-color]').setAttribute('content', '#090b28')
-	toggleSwitch.checked = true
-	localStorage.setItem('marcdownTheme', 'dark')
+  to ? clearTimeout(to) : (to = setTimeout(drawListBook, time));
+};
+searchBooks.addEventListener("input", () => debounce(drawListBook, 1000));
+document.addEventListener("DOMContentLoaded", () => {
+  drawChartBook("love");
+  drawChartBook("feminism");
+  drawChartBook("inspirational");
+  drawChartBook("authors");
+  drawChartBook("fiction");
+  drawChartBook("poetry");
+  drawChartBook("fantasy");
+  drawChartBook("romance");
+});
+let mainNavLinks = document.querySelectorAll(".scrolltoview");
+window.addEventListener("scroll", (event) => {
+  let fromTop = window.scrollY + 64;
+  mainNavLinks.forEach(({ hash, classList }) => {
+    let section = document.querySelector(hash);
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      classList.add("current");
+    } else {
+      classList.remove("current");
+    }
+  });
+});
+const getRandomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}40`;
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+if (localStorage.getItem("marcdownTheme") == "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+  document
+    .querySelector("meta[name=theme-color]")
+    .setAttribute("content", "#090b28");
+  toggleSwitch.checked = true;
+  localStorage.setItem("marcdownTheme", "dark");
 } else {
-	document.documentElement.setAttribute('data-theme', 'light')
-	document.querySelector('meta[name=theme-color]').setAttribute('content', '#ffffff')
-	toggleSwitch.checked = false
-	localStorage.setItem('marcdownTheme', 'light')
+  document.documentElement.setAttribute("data-theme", "light");
+  document
+    .querySelector("meta[name=theme-color]")
+    .setAttribute("content", "#ffffff");
+  toggleSwitch.checked = false;
+  localStorage.setItem("marcdownTheme", "light");
 }
-const switchTheme = ({
-	target
-}) => {
-	if (target.checked) {
-		document.documentElement.setAttribute('data-theme', 'dark')
-		document.querySelector('meta[name=theme-color]').setAttribute('content', '#090b28')
-		localStorage.setItem('marcdownTheme', 'dark')
-	} else {
-		document.documentElement.setAttribute('data-theme', 'light')
-		document.querySelector('meta[name=theme-color]').setAttribute('content', '#ffffff')
-		localStorage.setItem('marcdownTheme', 'light')
-	}
-}
-toggleSwitch.addEventListener('change', switchTheme, false)
-let startIndex = 0
+const switchTheme = ({ target }) => {
+  if (target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    document
+      .querySelector("meta[name=theme-color]")
+      .setAttribute("content", "#090b28");
+    localStorage.setItem("marcdownTheme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    document
+      .querySelector("meta[name=theme-color]")
+      .setAttribute("content", "#ffffff");
+    localStorage.setItem("marcdownTheme", "light");
+  }
+};
+toggleSwitch.addEventListener("change", switchTheme, false);
+let startIndex = 0;
 const next = (subject) => {
-	startIndex += 6
-	if (startIndex >= 0) {
-		document.getElementById(`${subject}-prev`).style.display = 'inline-flex'
-		drawChartBook(subject, startIndex)
-	} else {
-		document.getElementById(`${subject}-prev`).style.display = 'none'
-	}
-}
+  startIndex += 6;
+  if (startIndex >= 0) {
+    document.getElementById(`${subject}-prev`).style.display = "inline-flex";
+    drawChartBook(subject, startIndex);
+  } else {
+    document.getElementById(`${subject}-prev`).style.display = "none";
+  }
+};
 const prev = (subject) => {
-	startIndex -= 6
-	if (startIndex <= 0) {
-		startIndex = 0
-		drawChartBook(subject, startIndex)
-		document.getElementById(`${subject}-prev`).style.display = 'none'
-	} else {
-		document.getElementById(`${subject}-prev`).style.display = 'inline-flex'
-		drawChartBook(subject, startIndex)
-	}
-}
-const modal = document.querySelector('.modal')
-const trigger = document.querySelector('.trigger')
-const closeButton = document.querySelector('.close-button')
-const toggleModal = () => modal.classList.toggle('show-modal')
-const windowOnClick = ({
-	target
-}) => {
-	if (target === modal) {
-		toggleModal()
-	}
-}
-trigger.addEventListener('click', toggleModal)
-closeButton.addEventListener('click', toggleModal)
-window.addEventListener('click', windowOnClick)
-let pwaInstalled = localStorage.getItem('pwaInstalled') == 'yes'
-if (window.matchMedia('(display-mode: standalone)').matches) {
-	localStorage.setItem('pwaInstalled', 'yes')
-	pwaInstalled = true
+  startIndex -= 6;
+  if (startIndex <= 0) {
+    startIndex = 0;
+    drawChartBook(subject, startIndex);
+    document.getElementById(`${subject}-prev`).style.display = "none";
+  } else {
+    document.getElementById(`${subject}-prev`).style.display = "inline-flex";
+    drawChartBook(subject, startIndex);
+  }
+};
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+const toggleModal = () => modal.classList.toggle("show-modal");
+const windowOnClick = ({ target }) => {
+  if (target === modal) {
+    toggleModal();
+  }
+};
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+let pwaInstalled = localStorage.getItem("pwaInstalled") == "yes";
+if (window.matchMedia("(display-mode: standalone)").matches) {
+  localStorage.setItem("pwaInstalled", "yes");
+  pwaInstalled = true;
 }
 if (window.navigator.standalone === true) {
-	localStorage.setItem('pwaInstalled', 'yes')
-	pwaInstalled = true
+  localStorage.setItem("pwaInstalled", "yes");
+  pwaInstalled = true;
 }
 if (pwaInstalled) {
-	document.getElementById('installPWA').style.display = 'none'
+  document.getElementById("installPWA").style.display = "none";
 } else {
-	document.getElementById('installPWA').style.display = 'inline-flex'
+  document.getElementById("installPWA").style.display = "inline-flex";
 }
-let deferredPrompt = null
-window.addEventListener('beforeinstallprompt', (e) => {
-	deferredPrompt = e
-})
+let deferredPrompt = null;
+window.addEventListener("beforeinstallprompt", (e) => {
+  deferredPrompt = e;
+});
 async function installPWA() {
-	if (deferredPrompt) {
-		deferredPrompt.prompt()
-		deferredPrompt.userChoice.then(({
-			outcome
-		}) => {
-			if (outcome === 'accepted') {
-				console.log('Your PWA has been installed')
-			} else {
-				console.log('User chose to not install your PWA')
-			}
-			deferredPrompt = null
-		})
-	}
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(({ outcome }) => {
+      if (outcome === "accepted") {
+        console.log("Your PWA has been installed");
+      } else {
+        console.log("User chose to not install your PWA");
+      }
+      deferredPrompt = null;
+    });
+  }
 }
-window.addEventListener('appinstalled', (evt) => {
-	localStorage.setItem('pwaInstalled', 'yes')
-	pwaInstalled = true
-	document.getElementById('installPWA').style.display = 'none'
-})
+window.addEventListener("appinstalled", (evt) => {
+  localStorage.setItem("pwaInstalled", "yes");
+  pwaInstalled = true;
+  document.getElementById("installPWA").style.display = "none";
+});
